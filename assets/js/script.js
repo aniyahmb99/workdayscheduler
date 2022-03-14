6920d4f76764d778874b6f4b880b839694c9daaa
aniyahmb99 <aniyah_mb@yahoo.com>
2022-03-13 23:26:05 -0400

    working on save button text persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,74 +2,43 @@ var today = moment().format(`dddd, MMM Do YYYY`); // time display at top
 $("#currentDay").html(today);
 var hourEl = document.getElementById("#hour"); // time block colorcoding
 var textAreaEL = document.getElementsByClassName("description") // save to browser
-var savebuttonEl = document.getElementById("#btnsave") // for save button click event
+var savebuttonEl = document.getElementById("#btnSave") // for save button click event
 
-
-
-// SAVE TO BROWSER
-var saveBrowser = function () {
-    console.log(saveBrowser);
-    localStorage.setItem('description', json.stringify())
-    localStorage.getItem()
+// Save to local storage
+var saveLocal = function () {
+    var linkEl = Event.target.getAttribute('description');
+    var text = document.getElementById(linkEl).value;
+    localStorage.setItem(text, JSON.stringify());
 
 }
 
 
+// function colorTracker() {
+//     var timeNow = moment().hour();
+
+//     $(".time-block").each(function () {
+//         var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+//         if (blockTime < timeNow) {
+//             $(this).removeClass("future");
+//             $(this).removeClass("present");
+//             $(this).addClass("past");
+//         } else if (blockTime === timeNow) {
+//             $(this).removeClass("past");
+//             $(this).removeClass("future");
+//             $(this).addClass("present");
+//         } else {
+//             $(this).removeClass("present");
+//             $(this).removeClass("past");
+//             $(this).addClass("future");
+//         } console.log();
+//     }
+//     )
+// }
+
+// when save button is clicked, saveLocal() will save to local Storage
+savebuttonEl.addEventListener('click', saveLocal());
 
 
-
-
-
-
-
-savebuttonEl.addEventListener('click', saveBrowser());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function colorTracker() {
-    var timeNow = moment().hour();
-
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-        if (blockTime < timeNow) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        } else if (blockTime === timeNow) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        } else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        } console.log();
-    }
-    )
-}
-
-
-colorTracker();
-
-
-
+// colorTracker();
 
